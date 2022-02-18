d0be68c19d61c95365fbd5b5fd35ed019c1d4a36
Ana Flávia Rodrigues <78914063+Ana-F-Rodrigues@users.noreply.github.com>
2022-02-18 15:34:58 -0300

    Trabalhando com Formularios

---
+++ MinhaAppVS/Models/Filme.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MinhaDemoMVC.Models
+{
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Título é obrigatorio")]
+        [StringLength(60, MinimumLength =  3 , ErrorMessage = "O título precisa ter entre 3 ou 60 caracteres")]
+        public string Titulo { get; set; }
+
+        [DataType(DataType.DateTime, ErrorMessage = "Data em formato incorreto")]
+        [Required(ErrorMessage = "O campo Data de Lancamento é obrigatorio")]
+        [Display(Name = "Data de Lançamento")]
+        public DateTime DataLancamento { get; set; }
+
+        [StringLength(30,ErrorMessage = "Maximo de 30 caracteres")]
+        [Required(ErrorMessage = "Genero é obrigatorio")]
+        public string Genero { get; set; }
+
+        [Range(1,1000, ErrorMessage = "Valor de 1 a 1000")]
+        [Required(ErrorMessage = "Preencha o campo Valor")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Valor { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Avaliação")]
+        [Display(Name = "Avaliação")]
+        [RegularExpression(@"^[0-5]^$", ErrorMessage = "Somente Numeros")]
+        public int Avaliacao { get; set; }
+
+    }
+}
